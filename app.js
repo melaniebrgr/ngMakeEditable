f3a72f3d1e2bc22fbc09c1d6358a23a9af1e889f
Melanie Burger <melanie.brgr@gmail.com>
2016-06-13 19:30:22 -0400

    toggle class, editability on button click

--- app.js
+++ app.js
@@ -6,16 +6,33 @@ app.directive('makeEditable', function() {
 		templateUrl: 'make-editable.html',
 		transclude: true,
 		link: function(scope, element, attrs) {
+			let editableElement = angular.element(document.getElementById('editable'));
+			console.log(editableElement.attr('contenteditable'));
+
 			scope.editable = false;
 			scope.button = {
 				action: "Edit"
+			};
+
+			let turnOnEditing = function() {
+				scope.editable = true;
+				scope.button.action = 'Save';
+				editableElement.attr('contenteditable', true)
+			};
+			let turnOffEditing = function() {
+				scope.editable = false;
+				scope.button.action = 'Edit';
+				editableElement.attr('contenteditable', false)
+			};
+			scope.toggleEditability = function() {
+				scope.editable === false ? turnOnEditing() : turnOffEditing();
 			};
 		}
 	}
 });
 
 // -|- transclude content
-// --on button click, toggle editability
-// --on button click, change button text
-// --when editable, apply red class
+// -|-on button click, toggle editability
+// -|-on button click, change button text
+// -|-when editable, apply red class
 // --include attribute on at least two DOM elements
